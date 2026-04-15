00e2016cd1696e0c67197c8f5278eb9e7f024de7
ashikurrb <ashikurrb@outlook.com>
2026-04-15 21:34:16 +0600

    revert samesite setting for local mechine

--- src/modules/user/auth.controller.ts
+++ src/modules/user/auth.controller.ts
@@ -309,8 +309,7 @@ export const login = async (
     const cookieOptions = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      // sameSite: "strict" as const,
-      sameSite: "none" as const,
+      sameSite: "strict" as const,
     };
 
     res.cookie("refreshToken", refreshToken, {
